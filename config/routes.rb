5dbbb4f205a93fc9b63d0b6b570323c0042110b2
nemuo1208 <151502454+nemuo1208@users.noreply.github.com>
2023-11-28 00:52:45 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:"rooms#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
